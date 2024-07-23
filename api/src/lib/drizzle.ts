f53a090b52dd816f069428143b670fba244afd3c
Davi Reis <dreisss.dev@gmail.com>
2024-07-23 17:44:26 -0300

    fix(api): fixes in drizzle and postgres config

--- api/src/lib/drizzle.ts
+++ api/src/lib/drizzle.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import postgres from 'postgres'
 
-import * as schema from '@/db/schemas'
+import schema from '@db/schemas'
 
 let client: postgres.Sql
 
