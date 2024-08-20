90f59a94618c5a39d3e5b12e8e605a9cffa5135a
Davi Reis <dreisss.dev@gmail.com>
2024-08-20 12:12:04 -0300

    feat(scripts/setup/essencials davi): add obsidian install

--- scripts/setup/ESSENCIAIS ( DAVI ).ps1
+++ scripts/setup/ESSENCIAIS ( DAVI ).ps1
@@ -1,4 +1,5 @@
 winget install --id=WiresharkFoundation.Wireshark  -e;
 winget install --id=Discord.Discord  -e;
+winget install --id=Obsidian.Obsidian  -e;
 
 choco install git git-lfs gh httpie jq -y
